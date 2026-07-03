12b89e3b1d6faf00c1f858c508da29535b542268
LuisNaranjo2003 <naranjoluis2bgujac@gmail.com>
2026-07-02 19:33:24 -0500

    cambios en el inex.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,42 @@
 <?php
 
-require_once "../app/controllers/VehiculoController.php";
+$controller = $_GET['controller'] ?? 'vehiculo';
+$action = $_GET['action'] ?? 'index';
 
-$controller = new VehiculoController();
+switch ($controller) {
 
-$action = $_GET["action"] ?? "index";
+    case 'vehiculo':
+        require_once "../app/controllers/VehiculoController.php";
+        $controller = new VehiculoController();
+        break;
 
+    case 'cliente':
+        require_once "../app/controllers/ClienteController.php";
+        $controller = new ClienteController();
+        break;
+
+    case 'mecanico':
+        require_once "../app/controllers/MecanicoController.php";
+        $controller = new MecanicoController();
+        break;
+
+    case 'servicio':
+        require_once "../app/controllers/ServicioController.php";
+        $controller = new ServicioController();
+        break;
+
+    case 'orden':
+        require_once "../app/controllers/OrdenController.php";
+        $controller = new OrdenController();
+        break;
+
+    default:
+        die("Controlador no encontrado");
+}
+
+// Ejecutar acción si existe
 if (method_exists($controller, $action)) {
     $controller->$action();
 } else {
-    echo "Página no encontrada";
+    echo "Acción no encontrada";
 }
