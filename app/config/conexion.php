2588c369851166091b137cb611ad686350390124
LuisNaranjo2003 <naranjoluis2bgujac@gmail.com>
2026-07-08 17:07:12 -0500

    actuyalzacion de conexion para usar variables de entorno y habilitar SSL con Aiven

--- app/config/conexion.php
+++ app/config/conexion.php
@@ -1,18 +1,32 @@
 <?php
 
-class Conexion {
-
+class Conexion
+{
     public static function conectar()
     {
-        $host = "localhost";
-        $bd = "taller_mecanico";
-        $usuario = "root";
-        $pass = "";
+        $host = getenv("DB_HOST");
+        $usuario = getenv("DB_USER");
+        $pass = getenv("DB_PASSWORD");
+        $bd = getenv("DB_NAME");
+        $puerto = getenv("DB_PORT");
 
-        $conn = new mysqli($host, $usuario, $pass, $bd);
+        $conn = mysqli_init();
 
-        if ($conn->connect_error) {
-            die("Error de conexión: " . $conn->connect_error);
+        mysqli_ssl_set($conn, NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+
+        mysqli_real_connect(
+            $conn,
+            $host,
+            $usuario,
+            $pass,
+            $bd,
+            $puerto,
+            NULL,
+            MYSQLI_CLIENT_SSL
+        );
+
+        if (mysqli_connect_errno()) {
+            die("Error de conexión: " . mysqli_connect_error());
         }
 
         $conn->set_charset("utf8");
