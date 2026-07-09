dbf67694c3644e99b6acfa1a2dd4fba6c303402b
LuisNaranjo2003 <naranjoluis2bgujac@gmail.com>
2026-07-09 16:42:18 -0500

    actualziacion de conexion a base de datos con ssl

--- app/config/conexion.php
+++ app/config/conexion.php
@@ -4,15 +4,36 @@ class Conexion
 {
     public static function conectar()
     {
+        $host    = getenv("DB_HOST");
+        $bd      = getenv("DB_NAME");
+        $usuario = getenv("DB_USER");
+        $pass    = getenv("DB_PASSWORD");
+        $puerto  = getenv("DB_PORT");
+        $ca      = getenv("DB_SSL_CA");
 
-        $host = "localhost";
-        $bd = "taller_mecanico";
-        $usuario = "root";
-        $pass = "";
+        $conn = mysqli_init();
 
-        $conn = new mysqli($host, $usuario, $pass, $bd);
+        mysqli_ssl_set(
+            $conn,
+            null,
+            null,
+            $ca,
+            null,
+            null
+        );
 
-        if ($conn->connect_error) {
+        mysqli_real_connect(
+            $conn,
+            $host,
+            $usuario,
+            $pass,
+            $bd,
+            (int)$puerto,
+            null,
+            MYSQLI_CLIENT_SSL
+        );
+
+        if ($conn->connect_errno) {
             die("Error de conexión: " . $conn->connect_error);
         }
 
