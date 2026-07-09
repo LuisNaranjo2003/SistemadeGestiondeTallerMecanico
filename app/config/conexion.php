c33c174830c997639cdfcf54c2b13ed935cb6934
LuisNaranjo2003 <naranjoluis2bgujac@gmail.com>
2026-07-09 12:24:08 -0500

    cambios en conexion.php para usar credenciales locales beta

--- app/config/conexion.php
+++ app/config/conexion.php
@@ -1,16 +1,15 @@
 <?php
 
-class Conexion
-{
+class Conexion {
+
     public static function conectar()
     {
-        $host = getenv("DB_HOST");
-        $bd = getenv("DB_NAME");
-        $usuario = getenv("DB_USER");
-        $pass = getenv("DB_PASSWORD");
-        $puerto = getenv("DB_PORT");
+        $host = "localhost";
+        $bd = "taller_mecanico";
+        $usuario = "root";
+        $pass = "";
 
-        $conn = new mysqli($host, $usuario, $pass, $bd, $puerto);
+        $conn = new mysqli($host, $usuario, $pass, $bd);
 
         if ($conn->connect_error) {
             die("Error de conexión: " . $conn->connect_error);
