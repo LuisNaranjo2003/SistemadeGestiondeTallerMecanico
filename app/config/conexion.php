be3e003fa9ede0cf11ae5bdaab85a6ce86b74c27
LuisNaranjo2003 <naranjoluis2bgujac@gmail.com>
2026-07-09 13:18:57 -0500

    no va puerto

--- app/config/conexion.php
+++ app/config/conexion.php
@@ -4,23 +4,11 @@ class Conexion
 {
     public static function conectar()
     {
-<<<<<<< HEAD
-        /*$host = getenv("DB_HOST");
-        $bd = getenv("DB_NAME");
-        $usuario = getenv("DB_USER");
-        $pass = getenv("DB_PASSWORD");
-        $puerto = getenv("DB_PORT");*/
 
-=======
->>>>>>> 695394b654a2c4ded96e6dbe0e753e7428b7eb02
         $host = "localhost";
         $bd = "taller_mecanico";
         $usuario = "root";
         $pass = "";
-<<<<<<< HEAD
-        $puerto = 3306;
-=======
->>>>>>> 695394b654a2c4ded96e6dbe0e753e7428b7eb02
 
         $conn = new mysqli($host, $usuario, $pass, $bd);
 
